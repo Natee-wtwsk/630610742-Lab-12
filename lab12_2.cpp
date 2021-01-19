b6dd16f6519ea641c0d9304b9f22f0752b2423b9
Natee <gg977120@gmail.com>
2021-01-19 17:50:30 +0700

    write updateImage and showImage

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,35 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool Position[N][M])
+{
+    for(int i = 0;i < 72;i++) cout << "-";
+    cout << endl;
+
+    for(int j = 0;j < 30;j++)
+    {
+        cout << "|";
+        for(int i = 0;i < 70;i++)
+        {
+            if(Position[j][i] == 1)  cout << "*";
+            else cout << " ";
+        }
+        cout << "|";
+        cout << endl;
+    }
+
+    for(int i = 0;i < 72;i++) cout << "-";
+    cout << endl;
+}
+
+void updateImage(bool Position[N][M],int s,int x,int y)
+{
+    for(int i = 0;i < 30; i++)
+    {
+        for(int j = 0;j < 70;j++)
+        {
+            if(pow(pow(i-x,2)+pow(j-y,2),0.5) <= s-1) Position[i][j] = 1;
+        }
+    }
+}
