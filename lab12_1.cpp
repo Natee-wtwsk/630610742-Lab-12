f1b6dcae40726b5cfde9c1632b23b0adb3a1d187
Natee <gg977120@gmail.com>
2021-01-19 16:23:50 +0700

    write function stat

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,25 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int size,double B[])
+{
+    B[4] = A[0];  //set begin of Max
+    B[5] = A[0];  //set begin of Min
+    B[2] = 1; //set begin of Geometric Mean for multiples
+
+    for(int i = 0;i < size;i++)
+    {
+        B[0] += A[i];
+        B[1] += pow(A[i],2);
+        B[2] *= A[i];
+        B[3] += 1/A[i];
+        if(A[i] > B[4]) B[4] = A[i]; //find Max -done
+        if(A[i] < B[5]) B[5] = A[i]; //find Min -done
+    }
+
+    B[1] = pow((size*(B[1])-pow(B[0],2))/(pow(size,2)),0.5); //find Standard Deviation -done
+    B[2] = pow(B[2],(double)1/size); //find Geometric Mean - done
+    B[3] = size/B[3]; //find Harmonic Mean -done
+    B[0] = B[0]/size; //find Arithmetic Mean -done
+}
